828bce690391664392023e4ef27a0cd90b083d54
Fabian Streitel <streitel@cqse.eu>
2019-08-13 13:53:50 +0200

    log to console if git command fails

--- src/git.rs
+++ src/git.rs
@@ -1,7 +1,13 @@
+extern crate regex;
+
+use std::ffi::OsStr;
 use std::path::Path;
 use std::process::Command;
-use crate::utils::StrFromOutput;
+
 use crate::app::App;
+use crate::utils::StrFromOutput;
+
+use self::regex::Regex;
 
 pub struct Git<'a> {
     app: &'a App,
@@ -9,17 +15,28 @@ pub struct Git<'a> {
 }
 
 impl<'a> Git<'a> {
-    pub fn is_git(&self) -> bool {
-        let opt_stdout = Command::new("git")
-            .args(&["rev-parse", "--is-inside-work-tree"])
+    fn git(&self, args: &[&str]) -> Option<String> {
+        let result = Command::new("git")
+            .args(args)
             .output()
             .map_to_stdout();
 
+        match &result {
+            Err(error) => self.app.log(&format!("Git command {} failed: {}", args.join(", "), error.to_string())),
+            _ => (),
+        }
+
+        return result.ok();
+    }
+
+    pub fn is_git(&self) -> bool {
+        let opt_stdout = self.git(&["rev-parse", "--is-inside-work-tree"]);
+
         match opt_stdout {
-            Ok(ref stdout) if stdout.trim().eq("true") => {
+            Some(ref stdout) if stdout.trim().eq("true") => {
                 self.app.log("Current directory is in git");
-                return true
-            },
+                return true;
+            }
             _ => {
                 self.app.log("Current directory is not in git");
                 return false;
@@ -27,7 +44,19 @@ impl<'a> Git<'a> {
         }
     }
 
-    fn head_timestamp(&self) -> Option<String> {
+    pub fn head_timestamp(&self) -> Option<String> {
+        return self.git(&["--no-pager", "log", "-n1", "--format=\"%ct000\""]);
+    }
+
+    fn extract_single_branch(branch_text: &str) -> Option<String> {
+        let lines = branch_text.split_whitespace();
+        //lines.map(|line| )
         return None;
+    }
+
+    pub fn guess_branch(&self) -> Option<String> {
+        let opt_branches = self.git(&["branch", "--contains"]);
+        let branch_regex = Regex::new("^");
+        return opt_branches.and_then(|branch_text| Git::extract_single_branch(&branch_text));
     }
 }
