11d4deac888672b934e5e68261d02aa0d336c6e5
Fabian Streitel <streitel@cqse.eu>
2019-08-13 16:07:35 +0200

    add command line options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate clap;
+
+use clap::Arg;
+
 use crate::app::App;
 
 mod app;
@@ -6,9 +10,26 @@ mod svn;
 mod utils;
 
 fn main() {
-    let app = App::new(true);
+    let matches = clap::App::new("teamscale-timestamp")
+        .about("Tries to determine the value for the ?t= parameter when uploading external data to Teamscale. \
+            Run this command from within the working directory of your version control system checkout.")
+        .arg(Arg::with_name("verbose")
+            .short("v")
+            .long("verbose")
+            .help("Enable verbose output. Use this to debug what the tool is doing"))
+        .arg(Arg::with_name("branch")
+            .short("b")
+            .long("branch")
+            .takes_value(true)
+            .value_name("BRANCH")
+            .help("Pass a branch name to use for the upload (e.g. master or Main). \
+                Use this if automatic detection of the branch does not work"))
+        .get_matches();
 
-    let opt_branch = app.guess_branch();
+    let app = App::new(matches.is_present("verbose"));
+
+    let opt_branch = matches.value_of("branch").map(|branch| branch.to_string())
+        .or_else(|| app.guess_branch());
     let opt_timestamp = app.guess_timestamp();
 
     match opt_branch {
