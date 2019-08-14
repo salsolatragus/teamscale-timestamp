831e664c90f9559badce04a440b24a2be8c80ad0
Fabian Streitel <streitel@cqse.eu>
2019-08-14 07:49:57 +0200

    more env guesses for branch

--- src/app.rs
+++ src/app.rs
@@ -42,19 +42,46 @@ impl App {
         return None;
     }
 
+    fn env_variable(&self, name: &str) -> Option<String> {
+        let opt_value = std::env::var(name).ok();
+        self.log(&format!("${}={}", name, opt_value.clone().unwrap_or("".to_string())));
+        return opt_value;
+    }
+
     fn guess_branch_from_environment(&self) -> Option<String> {
-        let opt_branch = std::env::var("BRANCH").ok();
-        self.log(&format!("$BRANCH={}", opt_branch.clone().unwrap_or("".to_string())));
-        let opt_git_branch = std::env::var("GIT_BRANCH").ok();
-        self.log(&format!("$GIT_BRANCH={}", opt_git_branch.clone().unwrap_or("".to_string())));
-        return opt_git_branch.or(opt_branch);
+        // common names
+        return self.env_variable("BRANCH")
+            .or(self.env_variable("branch"))
+            .or(self.env_variable("GIT_BRANCH"))
+            // TeamCity https://stackoverflow.com/questions/13278615/is-there-a-way-to-access-teamcity-system-properties-in-a-powershell-script
+            // https://www.jetbrains.com/help/teamcity/predefined-build-parameters.html#PredefinedBuildParameters-Branch-RelatedParameters
+            .or(self.env_variable("build_branch"))
+            .or(self.env_variable("BUILD_BRANCH"))
+            // Jenkins https://github.com/jenkinsci/pipeline-model-definition-plugin/pull/91
+            .or(self.env_variable("BRANCH_NAME"))
+            // Azure Devops/TFS https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml
+            .or(self.env_variable("BUILD_SOURCEBRANCHNAME"))
+            // Circle CI https://circleci.com/docs/2.0/env-vars/#built-in-environment-variables
+            .or(self.env_variable("CIRCLE_BRANCH"))
+            // Travis CI https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+            .or(self.env_variable("TRAVIS_BRANCH"))
+            // BitBucket pipelines https://confluence.atlassian.com/bitbucket/environment-variables-794502608.html
+            .or(self.env_variable("BITBUCKET_BRANCH"))
+            // GitLab pipelines https://docs.gitlab.com/ee/ci/variables/predefined_variables.html
+            .or(self.env_variable("CI_MERGE_REQUEST_SOURCE_BRANCH_NAME"))
+            .or(self.env_variable("CI_COMMIT_REF_NAME"))
+            // Appveyor https://www.appveyor.com/docs/environment-variables/
+            .or(self.env_variable("APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH"))
+            .or(self.env_variable("APPVEYOR_REPO_BRANCH"));
     }
 
     pub fn guess_branch(&self) -> Option<String> {
         self.log("Trying to determine branch");
         return self.guess_branch_from_svn()
-            .or(self.guess_branch_from_git())
-            .or(self.guess_branch_from_environment());
+            // since guessing from a git commit is heuristic, we prefer to first check
+            // environment variables set by build runners
+            .or(self.guess_branch_from_environment())
+            .or(self.guess_branch_from_git());
     }
 
     pub fn guess_timestamp(&self) -> Option<String> {
